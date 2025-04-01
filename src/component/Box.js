aae015a6bf647ad5aeda37f29e373d2f01518f7c
minji <minji03421@naver.com>
2025-04-02 01:21:19 +0900

    alt수정

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,7 @@ const Box = (props) => {
   return (
     <div className="box" style={props.style}>
         <h1>{props.title}</h1>
-        {props.item ? (<img className="item-img" src={props.item.img} />) : (<div style={{ height: "100px" }}></div>)}
+        {props.item ? (<img className="item-img" src={props.item.img} alt={props.item.name || "아이템 이미지"} />) : (<div style={{ height: "100px" }}></div>)}
         <h2>{props.result}</h2>
     </div>
   )
